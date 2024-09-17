943407dd517fea31ae9fdd944a3f7a59178eee6f
vikaspatel7780 <120778869+vikaspatel7780@users.noreply.github.com>
2024-09-17 17:59:16 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
 // Initialize socket connection
-const socket = io('http://localhost:3001');
+const socket = io('https://socket-backend-cphg.onrender.com');
 
 function App() {
   const [message, setMessage] = useState('');
